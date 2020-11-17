26a7a3cbe0430f32beb06b2b399029e81f79527c
Ricardo Giaviti <ricardo.giaviti@sensedia.com>
2020-11-17 10:05:04 -0300

    First Commit

---
+++ src/main/kotlin/com/gh/rgiaviti/fiisparser/FiisParserApplication.kt
@@ -0,0 +1,11 @@
+package com.gh.rgiaviti.fiisparser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FiisParserApplication
+
+fun main(args: Array<String>) {
+	runApplication<FiisParserApplication>(*args)
+}
---
+++ src/test/kotlin/com/gh/rgiaviti/fiisparser/FiisParserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gh.rgiaviti.fiisparser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FiisParserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
